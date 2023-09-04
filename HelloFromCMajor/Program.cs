b4d0f23d54334e33c181ced60e64dcd5f67b1765
Pakkoyan <maksimenkobogdan12@gmail.com>
2023-09-04 13:54:01 +0600

    FirstCommit

---
+++ HelloFromCMajor/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Давайте познакомимся, ваше ФИО: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Приветствую тебя {0}", name);
+
